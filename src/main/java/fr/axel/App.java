afabb5504c9da9b5349ef278837bcf3b2bf7f566
Axel de Sablet <contact@axel.desablet.fr>
2015-02-18 19:55:41 +0100

    Add the parsing of the available Wifi mode choices

--- src/main/java/fr/axel/App.java
+++ src/main/java/fr/axel/App.java
@@ -1,13 +1,19 @@
 package fr.axel;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlOption;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import com.google.common.collect.ImmutableMap;
 
 public class App {
+	
 	public static void main( final String[] args ) throws Exception {
 		final WebClient webClient = new WebClient();
 		final String urlString = "http://192.168.1.1/setup.cgi?next_file=Wireless.htm";
@@ -20,7 +26,24 @@ public class App {
 		webClient.setCredentialsProvider(credentialsProvider);
 		
 		final HtmlPage page = webClient.getPage(url);
-		final String contents = page.asText();
-		System.out.println(contents);
+		
+		System.out.println(page.asXml());
+		
+		final DomElement selectDomElement = page.getElementByName("wl_gmode");
+		final HtmlSelect selectElement = (HtmlSelect) selectDomElement;
+		System.out.println(selectElement);
+		
+		final List<HtmlOption> options = selectElement.getOptions();
+		
+		final Map<String, String> optionMap;
+		{
+			final ImmutableMap.Builder<String, String> optionMapBuilder = ImmutableMap.builder();
+			for (final HtmlOption htmlOption : options) {
+				final String optionHumanName = htmlOption.asText();
+				final String optionHtmlValue = htmlOption.getValueAttribute();
+				optionMapBuilder.put(optionHumanName, optionHtmlValue);
+			}
+			optionMap = optionMapBuilder.build();
+		}
 	}
 }
